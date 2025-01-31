5f951d0cd4e14c5e8a48c2b1c1bfb1c8f87a2c70
erez252 <mishorierez@gmail.com>
2025-01-31 18:18:48 +0200

    Add files via upload

--- scripts/languageRedirect.js
+++ scripts/languageRedirect.js
@@ -1,10 +1,14 @@
 let userLang = navigator.language;
 userLang = userLang.split("-")[0];
 console.log(userLang);
+let lang;
 
 if(userLang == "he"){
     window.location.href = "./he"
+    localStorage.setItem('userLang', "he");
 }
 else{
     window.location.href = "./en" 
+    localStorage.setItem('userLang', "en");
 }
+
--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,9 @@ let userLang = navigator.language;
 userLang = userLang.split("-")[0];
 let temp;
 
+let lang = localStorage.getItem('userLang');
+console.log(lang);
+
 document.getElementById("C").onclick = function(){
     document.getElementById("deg").textContent = "°F"
 }
@@ -12,18 +15,31 @@ document.getElementById("F").onclick = function(){
 
 document.getElementById("button").onclick = function(){ //שלוחצים על הכפתור
     let input = Number(document.getElementById("input").value); //בודק מה הטפרטורה ששמת
-        if(document.getElementById("F").checked){ // F-אם להמיר ל
+    let pagelang = window.location.pathname.split("/").filter(Boolean)[0];
+    if (!pagelang) {
+        pagelang = "en";
+    }
+
+    if(document.getElementById("F").checked){ // F-אם להמיר ל
             temp = input * 9 / 5 + 32;  //עושה טת החישוב
             temp = Math.round(temp * 100) / 100; //מעגל
             document.getElementById("span").textContent = temp + "°F"; //מעדכן את הטקסט על המסך
-        }
-        else if(document.getElementById("C").checked){ // C-אם להמיר ל
+    }
+    else if(document.getElementById("C").checked){ // C-אם להמיר ל
             temp = (input - 32) * (5/9); //עושה טת החישוב
             temp = Math.round(temp * 100) / 100; //מעגל
             document.getElementById("span").textContent = temp + "°C"; //מעדכן את הטקסט על המסך
+    }
+    else{
+        if(pagelang == "he"){
+            temp = "לא בחרת טמפרורה להמיר אליו";
         }
         else{
             temp = "you did not select a temp to convert to";
-            document.getElementById("span").textContent = temp; //מעדכן את הטקסט על המסך
         }
+        document.getElementById("span").textContent = temp; //מעדכן את הטקסט על המסך
+    }
 }
+
+
+
