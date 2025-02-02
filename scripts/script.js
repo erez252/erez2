4d82f87b9694cba808a94828062021ef8454c581
erez252 <mishorierez@gmail.com>
2025-02-02 12:11:55 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let userLang = navigator.language;
-userLang = userLang.split("-")[1];
-const pagelang = window.location.pathname.split("/").filter(Boolean)[0];
+userLang = userLang.split("-")[0];
+const pagelang = window.location.pathname.split("/").filter(Boolean)[1];
 localStorage.setItem(`lang`, pagelang);
 console.log(localStorage.getItem(`lang`))
 
